a19e1312a1392f7002499e736b17202590c77f9f
João Vitor <jv.dossantos012@gmail.com>
2025-06-07 18:14:25 -0300

    feat: header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Header } from "@/components/header";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl text-green-400">Vlw Naje, sabe muito!</h1>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
     </div>
   );
 }
