f90d60447276b88318965199c7959d9b3143707b
João Vitor <jv.dossantos012@gmail.com>
2025-06-15 23:26:28 -0300

    feat: og image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
 import { LandingPage } from "@/templates/landing-page";
+import { url } from "inspector";
+import { title } from "process";
+
+export const metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-eight.vercel.app/og-image.jpg",
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
